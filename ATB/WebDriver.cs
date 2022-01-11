750da697d5c250978aaa3fe7af6c63fdef9cafc8
Ned Wilbur <Ned612@Gmail.com>
2022-01-11 10:24:16 -0500

    Start of webdriver

---
+++ ATB/WebDriver.cs
@@ -0,0 +1,43 @@
+﻿using ATB;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _ATB.Driver
+{
+    public class WebDriver
+    {
+        internal IWebDriver Driver { get; }
+        internal Config Config { get; }
+
+        public WebDriver(Config config)
+        {
+            Config = config;
+            Log.Debug($"Creating '{Config.Browser}' webdriver");
+
+            // Create Driver
+            Driver = Config.Browser switch
+            {
+                Browser.Chrome => new ChromeDriver(Config.BrowserOptions),
+                Browser.Edge => new EdgeDriver(Config.BrowserOptions),
+                Browser.Firefox => new FirefoxDriver(Config.BrowserOptions),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            // Configure Driver Settings
+            Driver.Manage().Window.Size = Config.Resolution;
+            Driver.Manage().Timeouts().ImplicitWait = Config.ImplicitWait;
+            Driver.Manage().Timeouts().AsynchronousJavaScript = Config.AsynchronousJavaScriptTimeout;
+            Driver.Manage().Timeouts().PageLoad = Config.PageLoadTimeout;
+
+            // TODO: Navigate to base URL
+            // TODO: Create Actions object to tie to this browser
+        }
+    }
+}
