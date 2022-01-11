a47991c2efffd970240dc680d8058b7ee3644e72
Ned Wilbur <Ned612@Gmail.com>
2022-01-11 10:23:46 -0500

    Added logger

---
+++ ATB/Log.cs
@@ -0,0 +1,30 @@
+﻿using ATB;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _ATB
+{
+    public static class Log
+    {
+        public enum Level { Debug, Info, Success, Warn, Error }
+
+        private static void LogMessage(Level level, string message) {
+            string logMessage = $"{level} | {message}";
+            Console.WriteLine(logMessage);
+        }
+
+        public static void Debug(string message) => LogMessage(Level.Debug, message);
+        public static void Info(string message) => LogMessage(Level.Info, message);
+        public static void Success(string message) => LogMessage(Level.Success, message);
+        public static void Warn(string message) => LogMessage(Level.Warn, message);
+        public static void Error(string message) => LogMessage(Level.Error, message);
+    }
+
+    // TODO: Add TC Name
+    // TODO: Add timings
+    // TODO: Add Allure reporting
+}
